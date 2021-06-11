586e48ee397938ab63284ea5b2e0461961ea16d9
Marek <258980@student.pwr.edu.pl>
2021-06-11 11:11:59 +0200

    zad5_1-258980

---
+++ build/CMakeFiles/program.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mateusz/Pulpit/Dron/faza1 janiec/src/Bryla.cpp" "/home/mateusz/Pulpit/Dron/faza1 janiec/build/CMakeFiles/program.dir/src/Bryla.cpp.o"
+  "/home/mateusz/Pulpit/Dron/faza1 janiec/src/Dron.cpp" "/home/mateusz/Pulpit/Dron/faza1 janiec/build/CMakeFiles/program.dir/src/Dron.cpp.o"
+  "/home/mateusz/Pulpit/Dron/faza1 janiec/src/Graniastoslup6.cpp" "/home/mateusz/Pulpit/Dron/faza1 janiec/build/CMakeFiles/program.dir/src/Graniastoslup6.cpp.o"
+  "/home/mateusz/Pulpit/Dron/faza1 janiec/src/Macierz3x3.cpp" "/home/mateusz/Pulpit/Dron/faza1 janiec/build/CMakeFiles/program.dir/src/Macierz3x3.cpp.o"
+  "/home/mateusz/Pulpit/Dron/faza1 janiec/src/Plaszczyzna.cpp" "/home/mateusz/Pulpit/Dron/faza1 janiec/build/CMakeFiles/program.dir/src/Plaszczyzna.cpp.o"
+  "/home/mateusz/Pulpit/Dron/faza1 janiec/src/Prostopadloscian.cpp" "/home/mateusz/Pulpit/Dron/faza1 janiec/build/CMakeFiles/program.dir/src/Prostopadloscian.cpp.o"
+  "/home/mateusz/Pulpit/Dron/faza1 janiec/src/Scena.cpp" "/home/mateusz/Pulpit/Dron/faza1 janiec/build/CMakeFiles/program.dir/src/Scena.cpp.o"
+  "/home/mateusz/Pulpit/Dron/faza1 janiec/src/Wektor3D.cpp" "/home/mateusz/Pulpit/Dron/faza1 janiec/build/CMakeFiles/program.dir/src/Wektor3D.cpp.o"
+  "/home/mateusz/Pulpit/Dron/faza1 janiec/src/example.cpp" "/home/mateusz/Pulpit/Dron/faza1 janiec/build/CMakeFiles/program.dir/src/example.cpp.o"
+  "/home/mateusz/Pulpit/Dron/faza1 janiec/src/lacze_do_gnuplota.cpp" "/home/mateusz/Pulpit/Dron/faza1 janiec/build/CMakeFiles/program.dir/src/lacze_do_gnuplota.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ENABLE_DOCTEST_IN_LIBRARY"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../include"
+  "../external/doctest/doctest"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
